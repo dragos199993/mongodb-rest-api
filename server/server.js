6e5e8ebed9196b15bbc83e1909a2b4181f59e055
Nedsilon <dragos199993@gmail.com>
2018-06-24 21:48:46 +0300

    added get route

--- server/server.js
+++ server/server.js
@@ -31,6 +31,16 @@ app.post("/todos", (req, res) => {
     });
 });
 
+app.get("/todos", (req, res) => {
+  Todo.find()
+    .then(todos => {
+      res.send({ todos });
+    })
+    .catch(err => {
+        res.status(400).send(err);
+    });
+});
+
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
   console.log(`Access: http://localhost:${port}`);
