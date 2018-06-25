fe0154f7bbe1928016de6deacced442c7a21e41d
Nedsilon <dragos199993@gmail.com>
2018-06-25 20:34:28 +0300

    added patch support

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const _ = require("lodash");
+
 const { ObjectID } = require("mongodb");
 // Db info
 const { mongoose } = require("./db/mongoose");
@@ -37,20 +39,73 @@ app.get("/todos", (req, res) => {
       res.send({ todos });
     })
     .catch(err => {
-        res.status(400).send(err);
+      res.status(400).send(err);
     });
 });
 
 app.get("/todos/:id", (req, res) => {
-    if(!ObjectID.isValid(req.params.id)){
-        res.status(404).send('Id was formatted incorrectly')
-    }
-    Todo.findById(req.params.id).then( todo => {
-        if(!todo)  return res.send("Id not found");
-        res.send(todo);
-    }).catch( err => res.status(404).send("ObjectID not valid"));
+  let id = req.params.id;
+  if (!ObjectID.isValid(id)) {
+    res.status(404).send("Id was formatted incorrectly");
+  }
+  Todo.findById(id)
+    .then(todo => {
+      if (!todo) return res.send("Id not found");
+      res.send(todo);
+    })
+    .catch(err => res.status(404).send("ObjectID not valid"));
 });
 
+app.delete("/todos", (req, res) => {
+  Todo.remove({})
+    .then(todo => {
+      if (!todo) return res.send("Id not found");
+      res.send(todo);
+    })
+    .catch(err => res.status(404).send("ObjectID.not valid"));
+});
+
+app.delete("/todos/:id", (req, res) => {
+  let id = req.params.id;
+  if (!ObjectID.isValid(id)) {
+    res.status(404).send("Id was formatted incorrectly");
+  }
+  Todo.findByIdAndRemove(id)
+    .then(todo => {
+      if (!todo) return res.send("Id not found");
+      res.send(todo);
+    })
+    .catch(err => res.status(404).send("ObjectID not validO"));
+});
+
+app.patch("/todos/:id", (req, res) => {
+  let id = req.params.id;
+  let body = _.pick(req.body, ["text", "completed"]);
+
+  if (!ObjectID.isValid(id)) {
+    res.status(404).send("Id was formatted incorrectly");
+  }
+
+  if (_.isBoolean(body.completed)) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(
+    id,
+    {
+      $set: body
+    },
+    { new: true }
+  )
+    .then(todo => {
+      if (!todo) res.status(404).send("Id not found");
+      res.send({todo});
+    })
+    .catch(err => res.status(404).send("Id was badly formatted"));
+});
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
